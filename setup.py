793863f02cd3ad007fb3fc6a6d8db99b09c2fc1f
Necdet Can Atesman <c.atesman@gmx.net>
2015-10-12 09:47:47 +0200

    Updated module dependencies and added MANIFEST.in

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.jsapi',
-    version='0.1.3',
+    version='0.1.4',
     description='Javascript API generator of The SCORE Framework',
     long_description=README,
     author='strg.at',
@@ -42,6 +42,6 @@ setup(
     install_requires=[
         'score.init',
         'score.ctx',
-        'score.js',
+        'score.js >= 0.1.3',
     ],
 )
