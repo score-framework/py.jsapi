4933e9e2338d7879705c6bb7ec47230f9623463b
Necdet Can Ateşman <can@strg.at>
2016-06-29 11:26:46 +0200

    Added __version__ variable

--- score/jsapi/__init__.py
+++ score/jsapi/__init__.py
@@ -27,5 +27,7 @@
 from ._init import init, ConfiguredJsapiModule
 from ._endpoint import Endpoint, UrlEndpoint, SafeException
 
+__version__ = '0.2.3'
+
 __all__ = ('init', 'ConfiguredJsapiModule', 'Endpoint', 'UrlEndpoint',
            'SafeException')
