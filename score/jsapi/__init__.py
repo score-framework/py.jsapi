028ccb2f952aa5f36668d1c23233eb92b0c2cfc6
Necdet Can Ateşman <can@strg.at>
2016-09-05 12:06:22 +0200

    Release: 0.3.4

--- score/jsapi/__init__.py
+++ score/jsapi/__init__.py
@@ -27,7 +27,7 @@
 from ._init import init, ConfiguredJsapiModule
 from ._endpoint import Endpoint, UrlEndpoint, SafeException
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 
 __all__ = ('init', 'ConfiguredJsapiModule', 'Endpoint', 'UrlEndpoint',
            'SafeException')
