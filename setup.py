2d308a8c78eb8a19cd98af3904b1444d180c55c9
Necdet Can Ateşman <can@atesman.at>
2018-08-21 15:27:19 +0200

    Release: 0.4.11

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.jsapi',
-    version='0.4.10',
+    version='0.4.11',
     description='Javascript API generator of The SCORE Framework',
     long_description=README,
     author='strg.at',
