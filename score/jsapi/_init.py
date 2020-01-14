11f5824743c1634dacd01caa327120a8729a2567
Necdet Can Ateşman <can@atesman.at>
2020-01-14 16:16:35 +0100

    Fix: Remove bogus asterisk characters
    Looks like the list of javascript keywords was containing footnontes,
    indicated by an asterisk character next to the name.

--- score/jsapi/_init.py
+++ score/jsapi/_init.py
@@ -100,12 +100,12 @@ def init(confdict, ctx, tpl, http):
 
 js_keywords = (
     'abstract', 'arguments', 'boolean', 'break', 'byte', 'case', 'catch',
-    'char', 'class*', 'const', 'continue', 'debugger', 'default', 'delete',
-    'do', 'double', 'else', 'enum*', 'eval', 'export*', 'extends*', 'false',
+    'char', 'class', 'const', 'continue', 'debugger', 'default', 'delete',
+    'do', 'double', 'else', 'enum', 'eval', 'export', 'extends', 'false',
     'final', 'finally', 'float', 'for', 'function', 'goto', 'if', 'implements',
-    'import*', 'in', 'instanceof', 'int', 'interface', 'let', 'long', 'native',
+    'import', 'in', 'instanceof', 'int', 'interface', 'let', 'long', 'native',
     'new', 'null', 'package', 'private', 'protected', 'public', 'return',
-    'short', 'static', 'super*', 'switch', 'synchronized', 'this', 'throw',
+    'short', 'static', 'super', 'switch', 'synchronized', 'this', 'throw',
     'throws', 'transient', 'true', 'try', 'typeof', 'var', 'void', 'volatile',
     'while', 'with', 'yield',)
 
